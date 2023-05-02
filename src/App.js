22a5d4c307dacf2c071c68f4afe662ff03b278b6
Davila99 <davilaeliseo453@gmail.com>
2023-05-01 18:48:41 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import About from './components/About';
 import Buscador from './components/Buscador';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Buscador/>
+    // <Buscador/>
+    <About/>
   );
 }
 
